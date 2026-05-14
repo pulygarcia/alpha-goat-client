a7bb2893a90e9de3a59dd016650b0ac015afb348
Puly Garcia <pulygarcia09@gmail.com>
2026-05-14 19:22:10 -0300

    chore(landing): remove about section, keep hero only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import { Hero } from './(marketing)/_components/Hero';
-import { AboutSection } from './(marketing)/_components/about/AboutSection';
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <AboutSection />
-    </>
-  );
+  return <Hero />;
 }
